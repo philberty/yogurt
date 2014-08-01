74b5b061e630dd265c8663efc34184c1b2d455de
redbrain <philip.herron@instil.co>
2014-08-01 22:27:06 +0100

    Angular hello world with requirejs

--- Yogurt/www/js/app.js
+++ Yogurt/www/js/app.js
@@ -11,7 +11,19 @@ require.config({
     }
 });
 
-define(['jquery', 'angular'], function ($, angular) {
-    var phonecatApp = angular.module('phonecatApp', []);	
-    console.log (phonecatApp.controller)
+define("app", ["angular"], function(angular) {
+    var app = angular.module("phonecatApp", []);
+    
+    app.controller('PhoneListCtrl', function ($scope) {
+	$scope.phones = [
+	    {'name': 'Nexus S',
+	     'snippet': 'Fast just got faster with Nexus S.'},
+	    {'name': 'Motorola XOOM™ with Wi-Fi',
+	     'snippet': 'The Next, Next Generation tablet.'},
+	    {'name': 'MOTOROLA XOOM™',
+	     'snippet': 'The Next, Next Generation tablet.'}
+	];
+    });
+
+    return app;
 });
