a6ecf8c2a14b3571750f0c71a810f8b0566ffeeb
Philip Herron <redbrain@gcc.gnu.org>
2014-08-02 02:01:47 +0100

    intial controllers working need bootstrapjs its v. ugly

--- Yogurt/www/js/app.js
+++ Yogurt/www/js/app.js
@@ -1,32 +1,30 @@
 require.config({
     paths: {
 	jquery: '/js/lib/jquery/dist/jquery',
-	angular: '/js/lib/angular/angular',
-	angularResource: '/js/lib/angular-resource/angular-resource'
+	angular: '/js/lib/angular/angular'
     },
     shim: {
 	'angular': {
 	    exports: 'angular'
-        },
-	'angularResource': {
-	   exports: 'angularResource'
-	}
+        }
     }
 });
 
-define("app", ["angular", "angularResource"], function(angular, angularResource) {
-    var app = angular.module("phonecatApp", []);
+define("app", ["angular"], function(angular) {
+    var app = angular.module("FringeApp", []);
     
-    app.controller('PhoneListCtrl', function ($scope) {
-	$scope.phones = [
-	    {'name': 'Nexus S',
-	     'snippet': 'Fast just got faster with Nexus S.'},
-	    {'name': 'Motorola XOOM™ with Wi-Fi',
-	     'snippet': 'The Next, Next Generation tablet.'},
-	    {'name': 'MOTOROLA XOOM™',
-	     'snippet': 'The Next, Next Generation tablet.'}
-	];
-    });
+    app.controller ('live', ['$scope', '$http',
+				 function($scope, $http) {
+				     $http.get ('/api/live').success(function(data) {
+					 $scope.data = data ['live_events']
+				     })
+				 }])
 
+    app.controller ('upcoming', ['$scope', '$http',
+				 function ($scope, $http) {
+				     $http.get ('/api/upcoming').success(function(data) {
+					 $scope.data = data ['']
+				     })
+				 }])
     return app;
 });
