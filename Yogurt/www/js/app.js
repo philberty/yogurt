b71ff7499378c4d0ebc5ff3dca5e6b78b8d7250a
redbrain <philip.herron@instil.co>
2014-08-21 23:22:52 +0100

    More stuff rendering on screen only vod screen left for 1.0 realse

--- Yogurt/www/js/app.js
+++ Yogurt/www/js/app.js
@@ -1,11 +1,17 @@
 require.config({
     paths: {
 	jquery: '/js/lib/jquery/dist/jquery',
-	angularRoute: '/js/lib/angular-route/angular-route',
+	bootstrap: '/js/lib/bootstrap/dist/js/bootstrap.min.js',
+
 	angular: '/js/lib/angular/angular',
-	bootstrap: '/js/lib/bootstrap/dist/js/bootstrap.min.js'
+	angularRoute: '/js/lib/angular-route/angular-route',
+	angularBootstrap: '/js/lib/angular-bootstrap/ui-bootstrap-tpls'
     },
     shim: {
+	'angularBootstrap': {
+	    deps: ['angular'],
+	    exports: 'angular'
+	},
 	'angularRoute': {
 	    deps: ['angular'],
 	    exports: 'angular'
@@ -18,8 +24,8 @@ require.config({
     deps: ['app']
 });
 
-define('app', ["jquery", "angular", "angularRoute"], function($, angular) {
-    var app = angular.module("FringeApp", ['ngRoute']);
+define('app', ["jquery", "angular", "angularBootstrap", "angularRoute"], function($, angular) {
+    var app = angular.module("FringeApp", ['ngRoute', 'ui.bootstrap']);
 
     app.controller("sidebar", function($scope, $location) {
 	$scope.$on('$locationChangeSuccess', function(event) {
@@ -27,58 +33,60 @@ define('app', ["jquery", "angular", "angularRoute"], function($, angular) {
 	})
     })
 
-    app.controller ('live', ['$scope', '$http',
-				 function($scope, $http) {
-				     $http.get ('/api/live').success(function(data) {
-					 $scope.data = data ['live_events']
-				     })
-				 }])
+    app.config(
+	['$routeProvider', function($routeProvider)
+	 {
+	     $routeProvider
+		 .when('/about', {
+		     templateUrl: 'about.html',
+		     controller: ''
+		 })
+		 .when('/upcoming', {
+		     templateUrl: 'listview.html',
+		     controller: 'upcoming'
+		 })
+		 .when('/live', {
+		     templateUrl: 'listview.html',
+		     controller: 'live'
+		 })
+		 .when('/streams', {
+		     templateUrl: 'streams.html',
+		     controller: 'streams'
+		 })
+		 .when('/vods', {
+		     templateUrl: 'vods.html',
+		     controller: 'vods'
+		 })
+		 .when('/', {
+		     redirectTo: "/about"
+		 })
+	 }
+	]
+    )
+    
+    app.controller ('upcoming', function ($scope, $http) {
+	$http.get ('/api/upcoming').success(function(data) {
+	    $scope.data = data ['events']
+	})
+	$scope.name = 'Upcoming Events'
+	$scope.oneAtATime = true;
+    })
 
-    app.controller ('upcoming', ['$scope', '$http',
-				 function ($scope, $http) {
-				     $http.get ('/api/upcoming').success(function(data) {
-					 $scope.data = data ['events']
-				     })
-				 }])
+    app.controller ('live', function ($scope, $http) {
+	$http.get ('/api/live').success(function(data) {
+	    $scope.data = data ['live_events']
+	})
+	$scope.name = 'Live Events'
+	$scope.oneAtATime = true;
+    })
 
-    app.controller ('streams', ['$scope', '$http',
-				 function ($scope, $http) {
-				     $http.get ('/api/streams').success(function(data) {
-					 $scope.data = data ['starcraft2']
-				     })
-				 }])
-
-    app.controller ('leagues', ['$scope', '$http',
-				function ($scope, $http) {
-				    $http.get ('/api/leagues').success(function(data) {
-					$scope.data = []
-					var offs = 0
-					for (i in data ['leagues']) {
-					    $http.get ('/api/league/' + data ['leagues'][i]).success(
-						function(league) {
-						    $scope.data [offs++] = league
-						}
-					    )
-					}
-				    })
-				}])
-
-    app.config(['$routeProvider',
-		function($routeProvider) {
-		    $routeProvider
-			.when('/about', {
-			    templateUrl: 'about.html',
-			    controller: ''
-			})
-			.when('/upcoming', {
-			    templateUrl: 'upcoming.html',
-			    controller: ''
-			})
-			.otherwise({
-			    redirectTo: "/about"
-			})
-		}])
-
+     app.controller ('streams', function ($scope, $http) {
+	$http.get ('/api/streams').success(function(data) {
+	    $scope.data = data ['starcraft2']
+	})
+	$scope.oneAtATime = true;
+    })
+    
     angular.bootstrap(document, ['FringeApp']);
     return app;
 });
