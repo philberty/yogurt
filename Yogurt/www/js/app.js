bfa22d8bcdd6f27dd1ceb3f9364cca1f1fe0ce91
redbrain <philip.herron@instil.co>
2014-08-21 21:18:11 +0100

    Nice dynamic single view page now

--- Yogurt/www/js/app.js
+++ Yogurt/www/js/app.js
@@ -1,19 +1,32 @@
 require.config({
     paths: {
 	jquery: '/js/lib/jquery/dist/jquery',
+	angularRoute: '/js/lib/angular-route/angular-route',
 	angular: '/js/lib/angular/angular',
-	bootstrap: '/js/lib/bootstrap/dist/js/bootstrap'
+	bootstrap: '/js/lib/bootstrap/dist/js/bootstrap.min.js'
     },
     shim: {
+	'angularRoute': {
+	    deps: ['angular'],
+	    exports: 'angular'
+	},
 	'angular': {
+	    deps: ['jquery'],
 	    exports: 'angular'
         }
-    }
+    },
+    deps: ['app']
 });
 
-define("app", ["angular"], function(angular) {
-    var app = angular.module("FringeApp", []);
-    
+define('app', ["jquery", "angular", "angularRoute"], function($, angular) {
+    var app = angular.module("FringeApp", ['ngRoute']);
+
+    app.controller("sidebar", function($scope, $location) {
+	$scope.$on('$locationChangeSuccess', function(event) {
+	    $scope.tab = $location.path()
+	})
+    })
+
     app.controller ('live', ['$scope', '$http',
 				 function($scope, $http) {
 				     $http.get ('/api/live').success(function(data) {
@@ -50,5 +63,22 @@ define("app", ["angular"], function(angular) {
 				    })
 				}])
 
+    app.config(['$routeProvider',
+		function($routeProvider) {
+		    $routeProvider
+			.when('/about', {
+			    templateUrl: 'about.html',
+			    controller: ''
+			})
+			.when('/upcoming', {
+			    templateUrl: 'upcoming.html',
+			    controller: ''
+			})
+			.otherwise({
+			    redirectTo: "/about"
+			})
+		}])
+
+    angular.bootstrap(document, ['FringeApp']);
     return app;
 });
